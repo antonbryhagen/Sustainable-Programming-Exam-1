cb16ddc45ca0ca5185ebaa33ff2431d50f955e5b
Gabriel <gabriel.thiman0007@stud.hkr.se>
2023-02-09 18:15:24 +0100

    Rework Example1

--- src/Example1/Main.java
+++ src/Example1/Main.java
@@ -1,18 +1,22 @@
 package Example1;
 
 import java.security.SecureRandom;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.OptionalDouble;
 
 public class Main {
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
+        Instant start = Instant.now();
         SecureRandom random = new SecureRandom();
-        OptionalDouble optDouble = random.ints(60000000, 1, 100).parallel()
+        OptionalDouble optDouble = random.ints(5000000, 1, 100)
+                //.parallel()
                 .average();
         double average = optDouble.getAsDouble();
+        Instant stop = Instant.now();
+        Duration timeToRun = Duration.between(start, stop);
+        System.out.println("Time to run " + timeToRun.toMillis());
         System.out.println("Average: " + average);
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println("Time: " + duration/1000000000 + " seconds");
+
     }
 }
--- src/Example2/Main.java
+++ src/Example2/Main.java
@@ -3,19 +3,20 @@ package Example2;
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 
 public class Main {
     public static void main(String[] args) {
         long startTime = System.nanoTime();
         SecureRandom random = new SecureRandom();
         int[] ints = random.ints(100000000, 1, 1001).toArray();
-        OptionalDouble optDouble = Arrays.stream(ints).parallel()
+        OptionalInt optDouble = Arrays.stream(ints).parallel()
                 .map(x -> x * 10 - 3000)
                 .filter(x -> x > 4000)
                 .distinct()
                 .sorted()
-                .average();
-        double average = optDouble.getAsDouble();
+                .findFirst();
+        int average = optDouble.getAsInt();
         System.out.println("Average: " + average);
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
